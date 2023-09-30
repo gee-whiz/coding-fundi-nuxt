49559648f7d3f413b54088a39b7f1e7b080ff16b
George Kapoya <gkapoya@gmail.com>
2023-09-30 15:22:11 +0300

    use async

--- pages/articleDetails/[id].vue
+++ pages/articleDetails/[id].vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>{{ article?.title }}</h1>
+  <h1>{{ data?.title }}</h1>
 </template>
 
 
@@ -12,27 +12,24 @@ import Article from '~/model/article';
 const store = useStore();
 const route = useRoute();
 const articleId = ref(route.params.id);
-const article = ref<Article | Article>();
-
-// Function to fetch and set article
-const fetchAndSetArticle = async () => {
-  await store.fetchArticleWithAssets(articleId.value as string);
-  article.value = store.getArticleById(articleId.value as string);
-  console.log(article.value);
-};
-
-// Watch for changes to articleId and re-fetch data
-watch(articleId, () => {
-  fetchAndSetArticle();
-});
-
-// Initialize
-fetchAndSetArticle();
 
 
-useSeoMeta({
-  description: () => `description: ${article.value?.title}`,
-  ogDescription: () => `ogDescription: ${article.value?.title}`,
-  ogImage: () => `ogImage: //images.ctfassets.net/svxuef11w26o/7mfsf7SwQGvYwVznGqHqh5/44f1a859ed4daaa1c874c01b042c76b9/pexels-realtoughcandycom-11035366.jpg'}`,
+const { error, data } = useAsyncData(
+  async () => {
+    store.fetchArticleWithAssets(articleId.value as string);
+    const article = store.getArticleById(articleId.value as string);
+    return article;
+  },
+  {
+    // uncomment this to make it work again
+    // server: false
+  },
+);
+
+
+useServerSeoMeta({
+  description: () => `description: ${data.value?.title}`,
+  ogDescription: () => `ogDescription: ${data.value?.title}`,
+  ogImage: () => `ogImage: ${data.value?.imageURL}`,
 })
 </script>
